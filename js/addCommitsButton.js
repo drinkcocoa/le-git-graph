8172484dfd55fa1bf8d68e284b7ec057f733a18d
drinkcocoa <theremingi@gmail.com>
2026-01-18 22:57:10 +0900

    icon fix
    Just Revert to original codes

--- js/addCommitsButton.js
+++ js/addCommitsButton.js
@@ -70,21 +70,17 @@ function addCommitsButton() {
         thisChild.addEventListener("click", closeCommitsTab);
     });
 
-    // Update icon
+    
+    // Update icon - one level deeper due to span wrapper
+    newButtonChild.children[0].children[0].setAttribute("class", "octicon octicon-git-commit");
+    newButtonChild.children[0].children[0].children[0].setAttribute("d", "M10.5 7.75a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zm1.43.75a4.002 4.002 0 01-7.86 0H.75a.75.75 0 110-1.5h3.32a4.001 4.001 0 017.86 0h3.32a.75.75 0 110 1.5h-3.32z");
+    newButtonChild.children[0].children[0].children[0].setAttribute("fill-rule", "evenodd");
+
+    // Remove Issues badge
     try {
-        if (newButtonChild.children[0]) {
-            newButtonChild.children[0].setAttribute("class", "octicon octicon-issue-opened UnderlineNav-octicon d-none d-sm-inline");
-            if (newButtonChild.children[0].children[0]) {
-                newButtonChild.children[0].children[0].setAttribute("d", "M10.5 7.75a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zm1.43.75a4.002 4.002 0 01-7.86 0H.75a.75.75 0 110-1.5h3.32a4.001 4.001 0 017.86 0h3.32a.75.75 0 110 1.5h-3.32z");
-                newButtonChild.children[0].children[0].setAttribute("fill-rule", "evenodd");
-            }
-            // Remove counter badge from cloned Issues tab
-            if (newButtonChild.children[0].children[1]) {
-                newButtonChild.children[0].removeChild(newButtonChild.children[0].children[1]);
-            }
-        }
-    } catch (e) {
-        // Icon update failed, continue with default
+        newButtonChild.children[0].children[0].removeChild(newButtonChild.children[0].children[0].children[1]);
+    } catch {
+        // No badge to remove
     }
 
     // Update label
